cc2236a5bbf1fbaeab9a8386f4eb3c777c93215b
PeCeT_full <mateusz49@gmail.com>
2014-08-08 23:22:22 +0200

    Uploading new app project: Przeliterowywacz

---
+++ Przeliterowywacz/Przeliterowywacz/MainWindow.xaml.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+using System.Threading;
+using System.IO;
+using NAudio;
+using NAudio.Wave;
+
+namespace Przeliterowywacz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const string configFileName = "Przeliterowywacz.ini"; // nazwa pliku konfiguracyjnego
+        // Konfiguracja domyślna
+        public bool includeDiacriticalChars = true;
+        public Int16 inputScheme = 0;
+        public string currentSpeechbank = "<domyślny>";
+
+        public MainWindow()
+        {
+            if (File.Exists(configFileName)) // Odczytywanie pliku konfiguracyjnego (o ile istnieje)
+            {
+                var sr = new StreamReader(configFileName);
+                string srLine;
+                while ((srLine = sr.ReadLine()) != null)
+                {
+                    if (srLine.Contains("IncludeDiactricalChars="))
+                        includeDiacriticalChars = Convert.ToBoolean(Convert.ToInt16(srLine.Substring(23)));
+                    else if (srLine.Contains("InputScheme="))
+                        inputScheme = Convert.ToInt16(srLine.Substring(12));
+                    else if (srLine.Contains("Speechbank="))
+                    {
+                        currentSpeechbank = srLine.Substring(11);
+                        if (currentSpeechbank == "<default>")
+                            currentSpeechbank = "<domyślny>";
+                    }
+                }
+                sr.Close();
+            }
+            else
+            {
+                var sw = new StreamWriter(new FileStream(configFileName, FileMode.CreateNew), Encoding.GetEncoding(1250));
+                sw.WriteLine("; Nie modyfikować tego pliku ręcznie!\r\n[Przeliterowywacz]\r\nIncludeDiactricalChars=1\r\nInputScheme=0\r\nSpeechbank=<default>"); // Spisywanie konfiguracji domyślnej na plik o stronie kodowej Windows-1250
+                sw.Close();
+            }
+            InitializeComponent();
+            if (inputScheme == 1)
+            {
+                TextBox1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFFFF"));
+                TextBox1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF000000"));
+            }
+            else if (inputScheme == 2)
+            {
+                TextBox1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF000000"));
+                TextBox1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00FF00"));
+            }
+        }
+
+        public bool firstTimeRunning = true, isQuiet = true;
+        OptionsWindow ow;
+        AboutWindow aw;
+        Thread T1;
+
+        public static void Concatenate(string outputFile, IEnumerable<string> sourceFiles)
+        {
+            byte[] buffer = new byte[1024];
+            WaveFileWriter waveFileWriter = null;
+
+            try
+            {
+                foreach (string sourceFile in sourceFiles)
+                {
+                    using (WaveFileReader reader = new WaveFileReader(sourceFile))
+                    {
+                        if (waveFileWriter == null)
+                            waveFileWriter = new WaveFileWriter(outputFile, reader.WaveFormat);
+                        else
+                        {
+                            if (!reader.WaveFormat.Equals(waveFileWriter.WaveFormat))
+                                throw new InvalidOperationException("Nie można powiązać plików dźwiękowych nie będących tego samego formatu!");
+                        }
+
+                        int read;
+                        while ((read = reader.Read(buffer, 0, buffer.Length)) > 0)
+                            waveFileWriter.WriteData(buffer, 0, read);
+                    }
+                }
+            }
+            finally
+            {
+                if (waveFileWriter != null)
+                    waveFileWriter.Dispose();
+            }
+        }
+
+        public string specifyFileName(int i, string toBeSaid)
+        {
+            string fileName = "Banki\\";
+            if (currentSpeechbank != "<domyślny>")
+                fileName += currentSpeechbank + '\\';
+            if (toBeSaid.Substring(i, 1) == " ")
+                fileName = null;
+            else if (toBeSaid.Substring(i, 1) == "." || toBeSaid.Substring(i, 1) == "," || toBeSaid.Substring(i, 1) == ":" || toBeSaid.Substring(i, 1) == ";" || toBeSaid.Substring(i, 1) == "!" || toBeSaid.Substring(i, 1) == "?" || toBeSaid.Substring(i, 1) == "'" || toBeSaid.Substring(i, 1) == "\"" || toBeSaid.Substring(i, 1) == "\\" || toBeSaid.Substring(i, 1) == "/" || toBeSaid.Substring(i, 1) == "%" || toBeSaid.Substring(i, 1) == "*" || toBeSaid.Substring(i, 1) == "|" || toBeSaid.Substring(i, 1) == "<" || toBeSaid.Substring(i, 1) == ">" || toBeSaid.Substring(i, 1) == "=")
+            {
+                if (includeDiacriticalChars)
+                {
+                    if (toBeSaid.Substring(i, 1) == ".")
+                        fileName += "kropka.wav";
+                    else if (toBeSaid.Substring(i, 1) == ",")
+                        fileName += "przecinek.wav";
+                    else if (toBeSaid.Substring(i, 1) == ":")
+                        fileName += "dwukropek.wav";
+                    else if (toBeSaid.Substring(i, 1) == ";")
+                        fileName += "srednik.wav";
+                    else if (toBeSaid.Substring(i, 1) == "!")
+                        fileName += "wykrzyknik.wav";
+                    else if (toBeSaid.Substring(i, 1) == "?")
+                        fileName += "pytajnik.wav";
+                    else if (toBeSaid.Substring(i, 1) == "'")
+                        fileName += "apostrof.wav";
+                    else if (toBeSaid.Substring(i, 1) == "\"")
+                        fileName += "cudzyslow.wav";
+                    else if (toBeSaid.Substring(i, 1) == "\\")
+                        fileName += "ukosnik_wsteczny.wav";
+                    else if (toBeSaid.Substring(i, 1) == "/")
+                        fileName += "ukosnik.wav";
+                    else if (toBeSaid.Substring(i, 1) == "%")
+                        fileName += "procent.wav";
+                    else if (toBeSaid.Substring(i, 1) == "*")
+                        fileName += "gwiazdka.wav";
+                    else if (toBeSaid.Substring(i, 1) == "|")
+                        fileName += "kreska_pionowa.wav";
+                    else if (toBeSaid.Substring(i, 1) == "<")
+                        fileName += "znak_mniejszosci.wav";
+                    else if (toBeSaid.Substring(i, 1) == ">")
+                        fileName += "znak_wiekszosci.wav";
+                    else if (toBeSaid.Substring(i, 1) == "=")
+                        fileName += "znak_rownosci.wav";
+                }
+                else
+                    fileName = null;
+            }
+            else
+                fileName += toBeSaid.Substring(i, 1).ToLower() + ".wav";
+            return fileName;
+        }
+
+        void F1(object txt)
+        {
+            string toBeSpelled = (string)txt, waveFileName = "";
+            SoundPlayer letterFile;
+            try
+            {
+                for (int i = 0; i < toBeSpelled.Length; i++)
+                {
+                    waveFileName = specifyFileName(i, toBeSpelled);
+                    if (isQuiet)
+                        break;
+                    if (waveFileName != null)
+                    {
+                        letterFile = new SoundPlayer((string)waveFileName);
+                        letterFile.PlaySync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Operacja przerwana, ponieważ wystąpił problem z następującym plikiem: " + AppDomain.CurrentDomain.BaseDirectory + waveFileName + ". " + ex.Message, "Błąd", MessageBoxButton.OK, MessageBoxImage.Hand);
+            }
+            isQuiet = true;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!firstTimeRunning)
+            {
+                if (isQuiet)
+                {
+                    if (ow != null)
+                        ow.Close();
+                    string textToSpell = TextBox1.Text;
+                    isQuiet = false;
+                    T1 = new Thread(F1);
+                    T1.Start(textToSpell);
+                }
+                else
+                    MessageBox.Show("Nie można teraz wykonać tej operacji, ponieważ poprzednio wywołana wciąż jest w toku. Aby móc to zrobić, należy poczekać na jej zakończenie.", MainSpeechWindow.Title, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+                MessageBox.Show("Wprowadź najpierw tekst do przeliterowania.", MainSpeechWindow.Title, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void TextBox1_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (firstTimeRunning)
+            {
+                TextBox1.Text = "";
+                firstTimeRunning = false;
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (ow != null)
+            {
+                ow.Close();
+                ow = null;
+            }
+            if (isQuiet)
+            {
+                ow = new OptionsWindow(configFileName, includeDiacriticalChars, currentSpeechbank);
+                ow.Show();
+            }
+            else
+                MessageBox.Show("Program zajęty. Aby móc to zrobić, należy najpierw poczekać na zakończenie operacji.", MainSpeechWindow.Title, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private async void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (!firstTimeRunning)
+            {
+                var FD = new Microsoft.Win32.SaveFileDialog();
+                FD.FileName = "*";
+                FD.DefaultExt = "wav";
+                FD.ValidateNames = true;
+                FD.Filter = "Pliki dźwiękowe (*.wav)|*.wav|Wszystkie pliki|*.*";
+
+                Nullable<bool> result = FD.ShowDialog();
+                if (result == true)
+                {
+                    string textToRecord = TextBox1.Text, waveFileName = "";
+                    List<string> filesToPlayList = new List<string>();
+                    bool wasFailed = false;
+
+                    for (int i = 0; i < textToRecord.Length; i++)
+                    {
+                        waveFileName = specifyFileName(i, textToRecord);
+                        if (waveFileName != null)
+                        {
+                            if (File.Exists(waveFileName))
+                                filesToPlayList.Add(waveFileName);
+                            else
+                            {
+                                wasFailed = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!wasFailed)
+                    {
+                        Concatenate(FD.FileName, filesToPlayList);
+                        if (File.Exists(FD.FileName))
+                        {
+                            StatusLabel.Content = "Zapisano.";
+                            await Task.Delay(5000);
+                            StatusLabel.Content = "";
+                        }
+                    }
+                    else
+                        MessageBox.Show("Plik nie został pomyślnie zapisany, ponieważ jeden lub więcej plików wymaganych do odtworzenia nie zostało znalezionych.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Hand);
+                }
+            }
+            else
+                MessageBox.Show("Wprowadź najpierw tekst do nagrania.", MainSpeechWindow.Title, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void MainSpeechWindow_Closed(object sender, EventArgs e)
+        {
+            isQuiet = true;
+            if (ow != null)
+                ow.Close();
+            if (aw != null)
+                aw.Close();
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            if (aw != null)
+            {
+                aw.Close();
+                aw = null;
+            }
+            aw = new AboutWindow();
+            aw.Show();
+        }
+    }
+}
---
+++ Przeliterowywacz/Przeliterowywacz/OptionsWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Przeliterowywacz
+{
+    /// <summary>
+    /// Interaction logic for OptionsWindow.xaml
+    /// </summary>
+    public partial class OptionsWindow : Window
+    {
+        MainWindow main = (MainWindow)Application.Current.MainWindow;
+        string configFileName;
+
+        public OptionsWindow(string configFileName, bool diacriticalOption, string speechbankName)
+        {
+            this.configFileName = configFileName;
+            InitializeComponent();
+            CheckBox1.IsChecked = diacriticalOption;
+            // Wyszukuje wszystkie podfoldery w folderze Banki i dodaje znalezione elementy do listy dostępnych banków mowy
+            ComboBoxItem cbi;
+            string[] folderNames = Directory.GetDirectories(AppDomain.CurrentDomain.BaseDirectory + "Banki\\");
+            for (int i = 0; i < folderNames.Length; i++)
+            {
+                string[] folderNameStructure = folderNames[i].Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                string folderName = "NIE ZNALEZIONO FOLDERU „Banki”";
+                for (int j = 0; j < folderNameStructure.Length - 1; j++)
+                    if (folderNameStructure[j] == "Banki")
+                    {
+                        folderName = folderNameStructure[j + 1];
+                        break;
+                    }
+                cbi = new ComboBoxItem();
+                cbi.Content = folderName;
+                ComboBox1.Items.Add(cbi);
+            }
+            // Sprawdza, czy ostatnio wybrany bank mowy dalej występuje w folderze „Banki”
+            for (int i = 0; i < ComboBox1.Items.Count; i++)
+            {
+                ComboBox1.SelectedIndex = i;
+                if (ComboBox1.SelectedItem.ToString().Substring(38) == speechbankName)
+                    break;
+                if (i == ComboBox1.Items.Count - 1)
+                {
+                    ComboBox1.SelectedIndex = 0;
+                    MessageBox.Show("Folder banku mowy o nazwie „" + speechbankName + "” nie został znaleziony, przywrócono ustawienie domyślne.", main.Title, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            }
+        }
+
+        private void DockButton1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CheckBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            main.includeDiacriticalChars = true;
+        }
+
+        private void CheckBox1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            main.includeDiacriticalChars = false;
+        }
+
+        private void StyleButton1_Click(object sender, RoutedEventArgs e)
+        {
+            main.TextBox1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7FFFFFC0"));
+            main.TextBox1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFFFF"));
+            main.inputScheme = 0;
+        }
+
+        private void StyleButton2_Click(object sender, RoutedEventArgs e)
+        {
+            main.TextBox1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFFFF"));
+            main.TextBox1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF000000"));
+            main.inputScheme = 1;
+        }
+
+        private void StyleButton3_Click(object sender, RoutedEventArgs e)
+        {
+            main.TextBox1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF000000"));
+            main.TextBox1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00FF00"));
+            main.inputScheme = 2;
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            main.currentSpeechbank = ComboBox1.SelectedItem.ToString().Substring(38);
+        }
+
+        private void DockButton2_Click(object sender, RoutedEventArgs e)
+        {
+            string speechbank = main.currentSpeechbank;
+            if (speechbank == "<domyślny>")
+                speechbank = "<default>";
+            var sw = new StreamWriter(new FileStream(configFileName, FileMode.Create), Encoding.GetEncoding(1250));
+            sw.WriteLine("; Nie modyfikować tego pliku ręcznie!\r\n[Przeliterowywacz]\r\nIncludeDiactricalChars=" + Convert.ToInt16(main.includeDiacriticalChars) + "\r\nInputScheme=" + main.inputScheme + "\r\nSpeechbank=" + speechbank); // Spisywanie konfiguracji domyślnej na plik o stronie kodowej Windows-1250
+            sw.Close();
+        }
+    }
+}
